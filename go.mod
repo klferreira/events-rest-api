20f8527b0054250e0fc857903542a879673a9fa2
Kaio Ferreira <kaiolf11@gmail.com>
2020-02-02 22:14:27 -0300

    chore: implementing mongodb wrapper interface

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module github.com/klferreira/events-rest-api
 go 1.13
 
 require (
+	github.com/globalsign/mgo v0.0.0-20181015135952-eeefdecb41b8
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/gorilla/mux v1.7.3
+	github.com/kr/pretty v0.2.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
 )
---
+++ pkg/mongo/client.go
@@ -0,0 +1,90 @@
+package mongo
+
+import (
+	"github.com/globalsign/mgo"
+	"github.com/globalsign/mgo/bson"
+)
+
+// Client is the interface to be implemented by MongoClient
+type Client interface {
+	Close()
+	FindOne(col string, query interface{}, result interface{}) error
+	FindAll(col string, query interface{}, result interface{}) error
+	Insert(col string, docs ...interface{}) error
+	Update(col string, sel interface{}, update interface{}) error
+	DeleteOne(col string, id string) error
+	DeleteAll(col string, sel interface{}) error
+}
+
+// Config stores the configuration required to setup a new MongoClient
+type Config struct {
+	Host     string
+	Database string
+	AuthDB   string
+	User     string
+	Pass     string
+}
+
+// MongoClient is an abstract wrapper for MongoDB operations
+type mongoClient struct {
+	dbname  string
+	session *mgo.Session
+}
+
+func NewMongoClient(url, dbname string) (*mongoClient, error) {
+	session, err := mgo.Dial(url)
+	if err != nil {
+		return nil, err
+	}
+
+	return &mongoClient{dbname, session}, nil
+}
+
+func (c *mongoClient) find(col string, query interface{}) *mgo.Query {
+	s := c.session.Copy()
+	defer s.Close()
+
+	return s.DB("").C(col).Find(query)
+}
+
+func (c *mongoClient) FindOne(col string, query interface{}, result interface{}) error {
+	return c.find(col, query).One(result)
+}
+
+func (c *mongoClient) FindAll(col string, query interface{}, result interface{}) error {
+	return c.find(col, query).All(result)
+}
+
+func (c *mongoClient) Insert(col string, docs ...interface{}) error {
+	s := c.session.Copy()
+	defer s.Close()
+
+	return s.DB("").C(col).Insert(docs...)
+}
+
+func (c *mongoClient) Update(col string, sel interface{}, update interface{}) error {
+	s := c.session.Copy()
+	defer s.Close()
+
+	return s.DB("").C(col).Update(sel, update)
+}
+
+func (c *mongoClient) DeleteOne(col string, id string) error {
+	s := c.session.Copy()
+	defer s.Close()
+
+	return s.DB("").C(col).RemoveId(bson.ObjectIdHex(id))
+}
+
+func (c *mongoClient) DeleteAll(col string, sel interface{}) error {
+	s := c.session.Copy()
+	defer s.Close()
+
+	_, err sion.DB("").C(col).RemoveAll(sel)
+	return err
+}
+
+// Close closes the client's MongoDB session
+func (c *mongoClient) Close() {
+	defer c.session.Close()
+}
